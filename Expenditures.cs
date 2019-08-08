5b4a4a2920d1a598b97b1db755833c567d7f8448
Zach Smith <zsmith126028@gmail.com>
2019-08-08 12:21:11 -0500

    add ints for utilities bills

--- Expenditures.cs
+++ Expenditures.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace BudgetApp
 {
-    class Expenditures
+    static class Expenditures
     {
+        public static void Utilities()
+        {
+            int totalUtilities;
+            int internetCable;
+            int phone;
+            int electric;
+            int mortgageOrRent;
+            int gas;
+            int water;
+
+            Console.Write("Enter your internet/cable bill: ");
+            internetCable = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your phone bill: ");
+            phone = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your electric bill: ");
+            electric = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your monthly rent or mortgage: ");
+            mortgageOrRent = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your gas (heating) bill: ");
+            gas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your water bill: ");
+            water = Convert.ToInt32(Console.ReadLine());
+
+            totalUtilities = internetCable + phone + electric + mortgageOrRent + gas + water;
+
+            Console.WriteLine("Your total monthly utilities cost is: " + totalUtilities);
+        }
     }
 }
